728bf739b2c04e13f9925b3a7f53effb9b658167
wituwitu <diego.wistuba@ug.uchile.cl>
2020-04-27 21:22:54 -0400

    los mensajes serán del tipo {"profundidad""número"}

--- beacon.py
+++ beacon.py
@@ -5,6 +5,14 @@ import socket
 import binascii
 import time
 
+
+def zfill(string, width):
+	if len(string) < width:
+		return ("0" * (width - len(string))) + string
+	else:
+		return string
+
+
 # init Sigfox for RCZ4 (Chile)
 sigfox = Sigfox(mode=Sigfox.SIGFOX, rcz=Sigfox.RCZ4)
 s = socket.socket(socket.AF_SIGFOX, socket.SOCK_RAW)
@@ -12,6 +20,7 @@ s.setblocking(True)
 s.setsockopt(socket.SOL_SIGFOX, socket.SO_RX, False)
 s.settimeout(10)
 
+c = 10
 submerged_time = 60
 
 # Wait for the beacon to be submerged
@@ -19,6 +28,7 @@ time.sleep(submerged_time)
 
 # Send 10 messages to the Sigfox network to test connectivity
 for i in range(100):
+	string = "{}{}".format(zfill(str(c), 3), zfill(i, 3))
 	payload = bytes(str(i).encode())
 	print("Sending...")
 	s.send(payload)
--- gateway.py
+++ gateway.py
@@ -2,7 +2,7 @@ import requests
 import base64
 
 
-def get_fragments(device_id, limit, username, password):
+def get_messages(device_id, limit, username, password):
 	key = "{}:{}".format(username, password)
 	auth = "Basic {}".format(base64.b64encode(key.encode()).decode("utf-8"))
 	url = "https://api.sigfox.com/v2/devices/{}/messages".format(device_id)
@@ -16,10 +16,28 @@ def get_fragments(device_id, limit, username, password):
 	params = {"limit": limit}
 	response = requests.request("GET", url, data=payload, headers=headers, params=params)
 	parsed = response.json()
-	byte_array = []
+	msg_array = []
 	values = parsed["data"]
 	for val in values:
-		byte_array.append(bytearray.fromhex(val["data"]))
-	return byte_array
+		msg_array.append(val["data"].decode())
+	return msg_array
 
-print(get_fragments("4d5a87", 100, "5dc465f4e833d941f6cc4cd3", "0f245424186a3148cc2d5bbb240509f9"))
+
+msg_array = get_messages("4d5a87", 100, "5dc465f4e833d941f6cc4cd3", "0f245424186a3148cc2d5bbb240509f9")
+
+# (here we assume that messages can't be received out of order (the last message payload is always greater))
+# Check if msg_array is naturally sorted from low to high
+# If not, there are messages from a previous execution and there have been losses
+# Filter msg_array so that it is sorted from low to high
+
+c = msg_array[-1][:3]
+
+
+def sorting_index(ls, i=0):
+	if ls == sorted(ls):
+		return i
+	else:
+		return sorting_index(ls[1:], i+1)
+
+
+sorted_array = msg_array[sorting_index(msg_array):]
